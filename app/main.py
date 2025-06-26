ba7ef2ddc7d7c05cfd75000851679a86973871cb
bryanadamg <bryanadamg@gmail.com>
2025-06-26 08:54:33 +0100

    feat: cpu temp fix and mc server status

--- app/main.py
+++ app/main.py
@@ -2,16 +2,19 @@
 from flask import Flask, jsonify, render_template
 import psutil
 import subprocess
+from mcstatus import JavaServer
+
 
 app = Flask(__name__)
 
 def get_cpu_temperature():
     try:
-        temp = subprocess.check_output(['vcgencmd', 'measure_temp']).decode()
-        print(temp)
-        return float(temp.split('=')[1].split("'")[0])
+        with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+            temp = int(f.read()) / 1000
+            print(temp)
+            return round(temp, 1)
     except Exception as e:
-        print(f"Error retrieveing CPU temperature: {e}")
+        print(f"Error reading temperature: {e}")
         return None
 
 def get_disk_usage():
@@ -29,6 +32,24 @@ def status():
         'ram_percent': psutil.virtual_memory().percent,
         'disk_usage': get_disk_usage()
     })
+
+@app.route("/api/minecraft")
+def minecraft_status():
+    try:
+        server = JavaServer.lookup("mc.tijel.xyz:28804")
+        status = server.status()
+        return jsonify({
+            "online": True,
+            "players": status.players.online,
+            "max_players": status.players.max,
+            "version": status.version.name,
+            "motd": status.description
+        })
+    except Exception as e:
+        return jsonify({
+            "online": False,
+            "error": str(e)
+        })
 
 @app.route("/coin-pusher")
 def coin_pusher():
